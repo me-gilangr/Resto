a10b64e7a152c91e35f606a048f4ac730da9e288
unknown <budi.ariyadi140886@gmail.com>
2020-09-22 19:31:23 +0700

    merger

--- public/js/app.js
+++ public/js/app.js
@@ -37331,8 +37331,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Gilang\Pribadi\PakBudiProject\MaResto\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Gilang\Pribadi\PakBudiProject\MaResto\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Latihan Laravel\Resto\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Latihan Laravel\Resto\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
